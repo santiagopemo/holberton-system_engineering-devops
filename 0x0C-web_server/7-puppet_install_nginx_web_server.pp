fb0f9879ec9e12bd2395ef5a060c31d1c65aea82
santiagopemo <santiagopemo@gmail.com>
2020-08-06 13:54:06 +0000

    added task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,24 @@
 # Install Nginx web server (w/ Puppet)
 package { 'nginx installed':
   ensure => installed,
-  name   => 'nginx',
+  name   => 'nginx'
 }
 
 file { '/var/www/html/index.html':
   ensure  => present,
   path    => '/var/www/html/index.html',
-  content => 'Holberton School',
+  content => 'Holberton School'
 }
 
-file_line { 'redirect':
+file_line { 'redirection':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
 }
 
-service { 'nginx listaning':
+service { 'nginx':
   ensure     => running,
   require    => Package['nginx'],
-  hasrestart => true,
+  hasrestart => true
 }
