e12f8f13ed2c2bdd2ae6fb27fe6c667b690b8bf9
santiagopemo <santiagopemo@gmail.com>
2020-08-14 19:55:48 +0000

    added task 2

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -15,7 +15,7 @@ file_line { 'custom_header':
   ensure  => present,
   path    => '/etc/nginx/sites-available/default',
   after   => 'listen 80 default_server;',
-  line    => 'add_header X-Served-By "$HOSTNAME";',
+  line    => "add_header X-Served-By $(hostname);",
   require => Package['nginx'],
 }
 
