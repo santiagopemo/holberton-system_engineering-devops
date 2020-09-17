777c03657dbb77767440338e47a8a64842d69ca6
santiagopemo <santiagopemo@gmail.com>
2020-09-17 23:06:19 +0000

    added task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -4,30 +4,37 @@ import requests
 from sys import argv
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, hot_list=[], words_dict={}):
     """
     Recursive function that queries the Reddit API, parses
     the title of all hot articles, and prints a sorted count
     of given keywords (case-insensitive, delimited by spaces
     """
-    recurse = __import__('2-recurse').recurse
-    try:
-        hot_list = recurse(subreddit)
-    except:
+    if words_dict == {}:
+        words_dict = dict.fromkeys(word_list, 0)
+    if hot_list == []:
+        after = "null"
+    else:
+        after = hot_list[-1].get('data').get("name")
+    url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(subreddit,
+                                                                 after)
+    headers = {"User-Agent": "santiagopemo"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
         return
-    if hot_list is None:
-        print()
-        return
-    words_dict = dict.fromkeys(word_list, 0)
+    response = response.json()
+    hot_list = response.get('data', {}).get('children', None)
     for post in hot_list:
         title = post.get('data').get('title').split()
         for word in word_list:
             for title_word in title:
                 if word.lower() == title_word.lower():
                     words_dict[word] += 1
-    sort_value = sorted(words_dict.items(), key=lambda i: i[::-1])
-    sort_alpha = sorted(sort_value, key=lambda i: i[1], reverse=True)
-    for word, value in sort_alpha:
-        if value != 0:
-            print('{}: {}'.format(word, value))
-    return
+    if response.get('data').get('after') is None:
+        sort_value = sorted(words_dict.items(), key=lambda i: i[::-1])
+        sort_alpha = sorted(sort_value, key=lambda i: i[1], reverse=True)
+        for word, value in sort_alpha:
+            if value != 0:
+                print('{}: {}'.format(word, value))
+        return
+    return count_words(subreddit, word_list, hot_list, words_dict)
