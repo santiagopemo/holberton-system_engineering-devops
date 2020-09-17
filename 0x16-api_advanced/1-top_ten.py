0929ff3a5f65e317499d166e3b8cb1e22f363001
santiagopemo <santiagopemo@gmail.com>
2020-09-17 17:43:00 +0000

    added task 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Top ten module"""
+import requests
+from sys import argv
+
+
+def top_ten(subreddit):
+    """
+    Function that queries the Reddit API and prints the titles
+    of the first 10 hot posts listed for a given subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {"User-Agent": "santiagopemo"}
+    response = requests.get(url, headers=headers).json()
+    hot_posts = response.get('data', {}).get('children', None)
+    if hot_posts == None:
+        print("None")
+    else:
+        for post in hot_posts:         
+            print(post.get('data').get('title'))
