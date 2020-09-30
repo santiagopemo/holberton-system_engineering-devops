892236840cffe0bd9deaeb27f6a38df324b7a228
santiagopemo <santiagopemo@gmail.com>
2020-09-30 15:37:39 +0000

    added comments, corrected path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,5 @@
+# Using strace, find out why Apache is returning a 500 error. Once you find the issue, fix it and then automate it using Puppet
 exec { 'corect-typos':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => ['/usr/local/bin', 'usr/bin '/bin'],
+  path    => ['/usr/local/bin', 'usr/bin', '/bin']
 }
