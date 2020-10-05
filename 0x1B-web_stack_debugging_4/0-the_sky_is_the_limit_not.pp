48bb9fd1667e638a872eae286c6d597c6959990c
santiagopemo <santiagopemo@gmail.com>
2020-10-05 17:30:55 +0000

    added web stack debugging 4 project

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,8 @@
 # increase the number of request nginx server can handle at time
 exec { 'increase-ULIMIT':
   command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3000\"/g" /etc/default/nginx',
-  path    => ['/bin']
+  path    => ['/bin'],
+  before  => Service['nginx']
 }
 service { 'nginx':
   ensure  => 'running',
